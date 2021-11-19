819eaac228cdcc2bdfc48b862d5d1e910af93565
DykunXV <dykunxv@gmail.com>
2021-11-19 20:09:17 +0100

    vorlesungsstuff

---
+++ Template/Source/01_Introduction.ts
@@ -0,0 +1,5 @@
+namespace Template {
+    export async function Introduction(): ƒS.SceneReturn {
+      console.log("FudgeStory Template Scene starting");
+    }
+  }
--- Template/Source/Main.ts
+++ Template/Source/Main.ts
@@ -4,11 +4,62 @@ namespace Template {
 
   console.log("FudgeStory template starting");
 
+  //define transitions
+  export let transition = {
+    clock: {
+      duration: 1,
+      alpha: "./Images/Transitions/puzzle.png",
+      edge: 1,
+    },
+    longerClock: {
+      duration: 2,
+      alpha: "./Images/Transitions/puzzle.png",
+      edge: 1,
+    }
+  };
+
+  //define audio
+  export let audio = {
+    backgroundTheme: "./Audio/BGM/TEST.mp3",
+  };
+
+  //define backgrounds
+  export let locations = {
+    bedroom: {
+      name: "Bedroom",
+      background: "./Images/Backgrounds/test.jpg"
+    }
+  };
+
+  //define characters
+  export let characters = {
+    narrator: {
+      name: "",
+    },
+    john: {
+      name: "John",
+      origin: ƒS.ORIGIN.BOTTOMCENTER,
+      pose: {
+        angry: "./Images/Characters/test/1.jpg",
+        happy: "",
+        upset: ""
+      }
+    }
+  };
+
+  export let dataForSave = {
+    
+  };
+
   window.addEventListener("load", start);
   function start(_event: Event): void {
     let scenes: ƒS.Scenes = [
-      { scene: Scene, name: "Scene" }
+      { scene: Scene, name: "Scene" },
+      { scene: Introduction, name: "Introduction" }
     ];
+
+    let uiElement: HTMLElement = document.querySelector("[type=interface]");
+    dataForSave = ƒS.Progress.setData(dataForSave, uiElement);
 
     // start the sequence
     ƒS.Progress.go(scenes);
